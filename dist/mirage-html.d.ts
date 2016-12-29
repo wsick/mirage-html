9473058f230c1a18bee5254a2a5d7fa829fb53de
Brad Sickles <brad_sickles@mckinsey.com>
2016-12-29 09:54:21 -0500

    build

--- dist/mirage-html.d.ts
+++ dist/mirage-html.d.ts
@@ -1,3 +1,10 @@
 declare module mirage.html {
     var version: string;
 }
+declare namespace mirage.html.sync {
+    interface IDOMMonitor {
+        start(): any;
+        stop(): any;
+    }
+    function NewDOMMonitor(target: Node, onNodeAdded: (node: Node, parent: Node) => void, onNodeRemoved: (node: Node, parent: Node) => void): IDOMMonitor;
+}
--- dist/mirage-html.js
+++ dist/mirage-html.js
@@ -5,5 +5,73 @@ var mirage;
         html.version = '0.1.0';
     })(html = mirage.html || (mirage.html = {}));
 })(mirage || (mirage = {}));
+var mirage;
+(function (mirage) {
+    var html;
+    (function (html) {
+        var sync;
+        (function (sync) {
+            function NewDOMMonitor(target, onNodeAdded, onNodeRemoved) {
+                function nodeHasLayoutAttr(node) {
+                    return !!node.getAttribute("data-layout");
+                }
+                function addElement(node, parent) {
+                    if (node.nodeType !== node.ELEMENT_NODE)
+                        return;
+                    if (!nodeHasLayoutAttr(node))
+                        return;
+                    onNodeAdded(node, parent);
+                }
+                function removeElement(node, parent) {
+                    if (node.nodeType !== node.ELEMENT_NODE)
+                        return;
+                    if (!nodeHasLayoutAttr(node))
+                        return;
+                    onNodeRemoved(node, parent);
+                }
+                var observer = new MutationObserver(function (mutations) {
+                    for (var i = 0; i < mutations.length; i++) {
+                        var mutation = mutations[i];
+                        if (mutation.type === "childList") {
+                            for (var j = 0; j < mutation.addedNodes.length; j++) {
+                                addElement(mutation.addedNodes[j], mutation.target);
+                            }
+                            for (var j = 0; j < mutation.removedNodes.length; j++) {
+                                removeElement(mutation.removedNodes[j], mutation.target);
+                            }
+                        }
+                        else if (mutation.type === "attributes") {
+                            if (!mutation.oldValue) {
+                                if (nodeHasLayoutAttr(mutation.target)) {
+                                    addElement(mutation.target, mutation.target.parentElement);
+                                }
+                            }
+                            else {
+                                if (!nodeHasLayoutAttr(mutation.target)) {
+                                    removeElement(mutation.target, mutation.target.parentElement);
+                                }
+                            }
+                        }
+                    }
+                });
+                return {
+                    start: function () {
+                        observer.observe(target, {
+                            childList: true,
+                            subtree: true,
+                            attributes: true,
+                            attributeOldValue: true,
+                            attributeFilter: ["data-layout"],
+                        });
+                    },
+                    stop: function () {
+                        observer.disconnect();
+                    },
+                };
+            }
+            sync.NewDOMMonitor = NewDOMMonitor;
+        })(sync = html.sync || (html.sync = {}));
+    })(html = mirage.html || (mirage.html = {}));
+})(mirage || (mirage = {}));
 
 //# sourceMappingURL=mirage-html.js.map
