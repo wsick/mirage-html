be6a745c7e37ce2a00f2e343fbd87bef15ecdebb
Brad Sickles <brad_sickles@mckinsey.com>
2017-01-02 17:32:53 -0500

    Fixing binder registry hoisting.

--- src/BinderRegistry.ts
+++ src/BinderRegistry.ts
@@ -13,49 +13,57 @@ namespace mirage.html {
         let roots: core.LayoutNode[] = [];
         binders = binders || [];
 
-        // roots tracks the list of roots that are contained within binders
-        function findHoistCandidate(binder: IBinder): core.LayoutNode {
-            let curRoot = binder.getRoot();
-            if (!curRoot)
-                return null;
-            let newRoot = curRoot;
-            // walk up the tree until we find a node without a parent
-            while (newRoot.tree.parent) {
-                newRoot = newRoot.tree.parent;
-            }
-            if (newRoot === curRoot)
-                return null;
-            // if this is already being tracked by a binder, this is not a candidate
-            if (roots.indexOf(newRoot) > -1)
-                return null;
-            return newRoot;
-        }
-
-        function hoist(addedRoots: core.LayoutNode[]): core.LayoutNode[] {
+        function hoist(addedRoots: core.LayoutNode[], destroyedRoots: core.LayoutNode[]) {
             // Hoist binders to the root
             // This is done to ensure that a binder is the true root
             // as a root may be slipped above an existing binder root
-            // Return added nodes that do not have binders as new root binder candidates
-            let missingBinderNodes: core.LayoutNode[] = addedRoots.slice(0);
+            // Added roots will be stripped of hoisted roots from existing binders
             for (var i = 0; i < binders.length; i++) {
                 let binder = binders[i];
-                let newRoot = findHoistCandidate(binder);
-                if (!newRoot) {
-                    // Since this binder has no candidate, let's destroy it
+                let curRoot = binder.getRoot();
+                if (!curRoot) {
+                    // Dead binder, destroy it
                     binders.splice(i, 1);
                     i--;
                     continue;
                 }
-                binder.setRoot(newRoot);
-                roots.push(newRoot);
-                // If this binder matches an added root,
-                // it should be excluded from missing binder nodes
-                let missingIndex = missingBinderNodes.indexOf(newRoot);
-                if (missingIndex > -1) {
-                    missingBinderNodes.splice(missingIndex, 1);
+                let newRoot = findRoot(curRoot);
+                if (curRoot !== newRoot) {
+                    replaceBinderRoot(binder, curRoot, newRoot, destroyedRoots);
                 }
+
+                // If this binder was taken over by newRoot and exists in addedRoots,
+                // we should remove so a duplicate binder isn't created
+                let existingIndex = addedRoots.indexOf(newRoot);
+                if (existingIndex > -1)
+                    addedRoots.splice(i, 1);
             }
-            return missingBinderNodes;
+        }
+
+        function findRoot(curRoot: core.LayoutNode): core.LayoutNode {
+            let newRoot = curRoot;
+            while (newRoot.tree.parent) {
+                newRoot = newRoot.tree.parent;
+            }
+            return newRoot;
+        }
+
+        function replaceBinderRoot(binder: IBinder, curRoot: core.LayoutNode, newRoot: core.LayoutNode, destroyedRoots: core.LayoutNode[]) {
+            // Replace this binder's root with newRoot
+
+            // If newRoot already has a binder, this binder should be destroyed
+            if (roots.indexOf(newRoot) > -1) {
+                destroyedRoots.push(curRoot);
+                return;
+            }
+
+            // stop tracking current root
+            let oldIndex = roots.indexOf(curRoot);
+            if (oldIndex > -1)
+                roots.splice(oldIndex, 1);
+
+            binder.setRoot(newRoot);
+            roots.push(newRoot);
         }
 
         function create(nodes: core.LayoutNode[]) {
@@ -68,29 +76,33 @@ namespace mirage.html {
             }
         }
 
-        function adjustDestroyed(destroyedRoots: core.LayoutNode[]) {
+        function destroy(destroyedRoots: core.LayoutNode[]) {
             for (var i = 0; i < binders.length; i++) {
-                let binder = binders[i];
-                let curRoot = binder.getRoot();
-                let index = destroyedRoots.indexOf(curRoot);
+                let index = destroyedRoots.indexOf(binders[i].getRoot());
                 if (index > -1) {
-                    // Root was destroyed, let's destroy this binder
-                    binders.splice(index, 1);
+                    destroyBinder(index);
                     i--;
-                    // Stop tracking this root as bound
-                    binder.setRoot(null);
-                    let trackIndex = roots.indexOf(curRoot);
-                    if (trackIndex > -1)
-                        roots.splice(trackIndex, 1);
                 }
+            }
+        }
+
+        function destroyBinder(index: number) {
+            let binder = binders.splice(index, 1)[0];
+            let curRoot = binder.getRoot();
+            binder.setRoot(null);
+            if (curRoot) {
+                // Stop tracking this root as bound
+                let trackIndex = roots.indexOf(curRoot);
+                if (trackIndex > -1)
+                    roots.splice(trackIndex, 1);
             }
         }
 
         return {
             update(addedRoots: core.LayoutNode[], destroyedRoots: core.LayoutNode[]) {
-                let missingBinderNodes = hoist(addedRoots);
-                create(missingBinderNodes);
-                adjustDestroyed(destroyedRoots);
+                hoist(addedRoots, destroyedRoots);
+                create(addedRoots);
+                destroy(destroyedRoots);
             },
         };
     }
